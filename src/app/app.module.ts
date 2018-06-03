96d7368a0004be2a2fba1390b718c26fc9ef0cad
Grady Stewart <gtstewar@ncsu.edu>
2018-06-03 14:33:30 -0400

    added player object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import {PlayerService} from './services/player.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PlayerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/player.ts
@@ -0,0 +1,6 @@
+export class Player {
+  player_name: string;
+  player_id: number;
+
+  constructor() {}
+}
