64b91f994906aeefac8e8acc53c9efc93cc26ff9
Grady Stewart <gtstewar@ncsu.edu>
2018-06-20 20:49:31 -0400

    got player home page working better, made player names clickable

---
+++ src/app/home/home-team/home-team-link/home-team-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-team-link',
+  templateUrl: './home-team-link.component.html',
+  styleUrls: ['./home-team-link.component.css']
+})
+export class HomeTeamLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
